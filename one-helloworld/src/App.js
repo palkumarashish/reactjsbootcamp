9f76ac57be904a992baa304c70790d89b0ac6956
Ashish Kr PAL <74855648+palkumarashish@users.noreply.github.com>
2021-02-03 15:31:30 +0530

    final commit
    This project belong to counter step

--- one-helloworld/src/App.js
+++ one-helloworld/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import "./App.css";
 
-function App() {
-  return (
+function App(){
+
+  const [count, setCount] = useState(0)
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Happy Learning ReactJs App Thanking you. Jordan  Sir ,
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>Counter app using State</h1>
+    </header>
+    <h2>Current value of count is {count}</h2>
+    <button onClick= {() => setCount(0)}>Reset Counter</button>
+    <button onClick={ () => setCount (count +1)}>Increae Counter</button>
+    <button onClick={ () => setCount (count -1)}>Decrease Counter</button>
+    
+      
     </div>
-  );
+    
+    
+
+    
+    );
+  
 }
+
 
 export default App;
